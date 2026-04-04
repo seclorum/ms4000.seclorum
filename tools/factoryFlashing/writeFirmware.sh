aba9ced4ed9dc36f594938cc9db59a8fdc2d57c0
Philipp Tiefenbacher <wizards23+github@gmail.com>
2026-04-04 07:22:30 +0200

    add instructions for flashing "stubborn" (aka bricked) shifters :) Hang in there please & Never give up!

--- tools/factoryFlashing/writeFirmware.sh
+++ tools/factoryFlashing/writeFirmware.sh
@@ -12,4 +12,22 @@ fi
 echo "Firmware Name: " $firmwareName
 ../esptool-ck/esptool -vv -cd ck -cb 921600 -cp $1  -cf $firmwareName
 
+## If you encounter a stubborn shifter, then comment out the line with the 921600
+## and try flashing using decreasing baud rates. M<aybe make some tea since
+## this might take some time...
+## This method usually works to revive "bricked" shifters :)
 
+# ../esptool-ck/esptool -vv -cd ck -cb 460800 -cp $1  -cf $firmwareName
+# ../esptool-ck/esptool -vv -cd ck -cb 115200 -cp $1  -cf $firmwareName
+# ../esptool-ck/esptool -vv -cd ck -cb 57600 -cp $1  -cf $firmwareName
+# ../esptool-ck/esptool -vv -cd ck -cb 38400 -cp $1  -cf $firmwareName
+# ../esptool-ck/esptool -vv -cd ck -cb 19200 -cp $1  -cf $firmwareName
+# ../esptool-ck/esptool -vv -cd ck -cb 9600 -cp $1  -cf $firmwareName
+
+
+## If this still does not work and you feel like giving up:
+## *** Always remember - it will get better again, I promise *hug* :) ***
+## Never give up! And if you realy don't know what to do just call a friend.
+## Don't worry about not having called for soooo long. Just call and you will
+## find out that all humans love getting in touch again after a long 
+## long time :) Just talk whatever comes to mind :)
